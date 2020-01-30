60f34c1a31cbe1cedd985825c4dd3cdde52c61bb
ytsqkl <58875910+ytsqkl@users.noreply.github.com>
2020-01-30 09:01:24 +0800

    Update keyword.js

--- static/js/keyword.js
+++ static/js/keyword.js
@@ -56,57 +56,7 @@ $(function () {
       openSearch(keyword)
       return;
     }
-    // TODO 上下键选择待选答案
-    var bl = moveChange(event);
-    if(bl){
-    	keywordChange(keyword);
-    }
-  }).on('blur', function () {
-    $('#search_result').hide();
-  }).on('focus', function () {
-    var keyword = $(this).val();
-    keywordChange(keyword);
-  });
   
-  function moveChange(e){
-		var k = e.keyCode || e.which;
-		var bl = true;
-		switch(k){
-			case 38:
-				rowMove('top');
-				bl = false;
-				break;
-			case 40:
-				rowMove('down');
-				bl = false;
-				break;
-		}
-		return bl;
-	}
-  function rowMove(move){
-  	var search_result = $('#search_result');
-  	var hove_li = null;
-  	search_result.find('.result-item').each(function(){
-  		if($(this).hasClass('active')){
-  			hove_li = $(this).index();
-  		}
-  	});
-  	if(move == 'top'){
-  		if(hove_li==null){
-	  		hove_li = search_result.find('.result-item').length-1;
-	  	}else{
-	  		hove_li--;
-	  	}
-  	}else if(move == 'down'){
-  		if(hove_li==null){
-	  		hove_li = 0;
-	  	}else{
-	  		hove_li==search_result.find('.result-item').length-1?(hove_li=0):(hove_li++);
-	  	}
-  	}
-  	search_result.find('.active').removeClass('active');
-  	search_result.find('.result-item').eq(hove_li).addClass('active');
-  }
 
   function keywordChange(keyword) {
     if (keyword === '') {
@@ -149,22 +99,7 @@ $(function () {
   });
 
  
-  // 推荐结果跳转
-  $('#search_result').on('click', 'li', function () {
-    var word = $(this).text();
-    $('#search_keyword').val(word);
-    openSearch(word);
-    $('#search_result').hide();
-  });
-
-  $(document).on(EVENT_CLEAR_KEYWORD, function () {
-    $('#clear_keyword').hide();
-    $('#search_result').hide();
-  });
-  $(document).on(EVENT_SEARCH, function (e, keyword) {
-    getSearchResult(keyword);
-  });
-
+ 
   // 获取搜索引擎类型
   function getSeachType() {
     return $('#search_logo').data('type');
